0952f1cdc5ad561ee95b7b15afacdc03672690a7
H8404 <h8404@student.jamk.fi>
2016-09-28 10:21:40 +0300

    Viikkotehtävä 3 on valmis

---
+++ Saitti/VK3.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="VK3.aspx.cs" Inherits="VK3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Valitse syntymäpäiväsi!</h2>
+            <asp:DropDownList id="drpCalMonth" Runat="Server" OnSelectedIndexChanged="Set_Calendar" AutoPostBack="true"></asp:DropDownList>
+            <asp:DropDownList id="drpCalYear" Runat="Server" OnSelectedIndexChanged="Set_Calendar" AutoPostBack="true"></asp:DropDownList>
+            <asp:Calendar ID="cdBirthday" runat="server" SelectionMode="Day" OnSelectionChanged="cdBirthday_SelectionChanged"></asp:Calendar>
+            <br />
+            <asp:Label ID="lbToday" runat="server"></asp:Label>
+            <br />
+            <asp:Label ID="lbBirthday" runat="server"></asp:Label>
+            <br />
+            <asp:Label ID="lbFinal" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/VK3.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class VK3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Populate_MonthList();
+            Populate_YearList();
+        }
+    }
+
+    protected void Populate_MonthList()
+    {
+        var dtf = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat;
+        for (int i = 1; i <= 12; i++)
+            drpCalMonth.Items.Add(new ListItem(dtf.GetMonthName(i), i.ToString()));
+        drpCalMonth.Items.FindByValue(DateTime.Now.Month.ToString()).Selected = true;
+    }
+
+
+    protected void Populate_YearList()
+    {
+        for (int intYear = DateTime.Now.Year - 60; intYear <= DateTime.Now.Year; intYear++)
+        {
+            drpCalYear.Items.Add(intYear.ToString());
+        }
+        drpCalYear.Items.FindByValue(DateTime.Now.Year.ToString()).Selected = true;
+    }
+
+    protected void cdBirthday_SelectionChanged(object sender, EventArgs e)
+    {
+        lbToday.Text = "Tänään on: " + cdBirthday.TodaysDate.ToShortDateString();
+        lbBirthday.Text = "Syntymäpäiväsi on: " + cdBirthday.SelectedDate.ToShortDateString();
+        DateTime startDay = cdBirthday.SelectedDate;
+        DateTime endDay = cdBirthday.TodaysDate;
+        lbFinal.Text = "Valitun päivän ja tämän päivän erotus on: " + (endDay - startDay).TotalDays;
+    }
+
+    protected void Set_Calendar(object Sender, EventArgs e)
+    {
+        int year = int.Parse(drpCalYear.SelectedValue);
+        int month = int.Parse(drpCalMonth.SelectedValue);
+        cdBirthday.VisibleDate = new DateTime(year, month, 1);
+    }
+}
