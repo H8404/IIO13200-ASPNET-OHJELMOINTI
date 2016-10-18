42ebe414c340f87dcfabc85d037883e842b48fb2
H8404 <h8404@student.jamk.fi>
2016-10-18 14:13:13 +0300

    Vk8 aloitettu

---
+++ Saitti/vk8.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/finkino.master" AutoEventWireup="true" CodeFile="vk8.aspx.cs" Inherits="vk8" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:XmlDataSource ID="myDataSource" runat="server"></asp:XmlDataSource>
+    <div>
+        <asp:ListBox ID="lbCity" runat="server" OnSelectedIndexChanged="lbCity_SelectedIndexChanged"></asp:ListBox>
+    </div>
+    <asp:Label runat="server" ID="lbMsg"></asp:Label>
+</asp:Content>
+
---
+++ Saitti/vk8.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class vk8 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            GetCities();
+        }
+    }
+
+    protected void GetCities()
+    {
+        try
+        {
+            string url = @"http://www.finnkino.fi/xml/TheatreAreas/";
+            myDataSource.DataFile = url;
+            string name;
+            string id;
+            XmlDocument doc = new XmlDocument();
+            doc = myDataSource.GetXmlDocument();
+            XmlNodeList nodes = doc.SelectNodes("/TheatreAreas/TheatreArea");
+            foreach (XmlNode n in nodes)
+            {
+                name = n["Name"].InnerText;
+                id = n["ID"].InnerText;
+                lbCity.Items.Add(new ListItem(name,id));
+            }
+        }
+        catch (Exception ex)
+        {
+            lbMsg.Text = ex.Message;
+        }
+    }
+
+    protected void GetMovies()
+    {
+        string url = @"http://www.finnkino.fi/xml/Schedule/?area=1015&dt=18.10.2016";
+        myDataSource.DataFile = url;
+    }
+
+    protected void lbCity_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string id = lbCity.SelectedValue;
+        string day = DateTime.Now.ToShortDateString();
+        string url = @"http://www.finnkino.fi/xml/Schedule/?area=" + id + "&dt=";
+        lbMsg.Text = "joo";
+        //myDataSource.DataFile = url;
+    }
+}
